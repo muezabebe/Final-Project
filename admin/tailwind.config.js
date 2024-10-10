36dcc40f9c75db064b3e09d658f6484feff0e305
muezabebe <muezabebeab@gmail.com>
2024-10-10 11:51:41 +0300

    n+1th commit

---
+++ admin/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // Ensure index.html is included
+    './src/**/*.{js,ts,jsx,tsx}', // Match JavaScript and TypeScript files inside the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
